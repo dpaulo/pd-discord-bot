73063ccbdb49307bda1bda0f78f8bbcf53ede7d7
Dovydas Paulauskas <dovydasp123@gmail.com>
2021-04-20 21:44:03 +0300

    Added SQLite 3 to the dependancies, fully functioning and finished CS:GO Item Price module

--- src/components/csgo-skin-price/CurrentPrice.js
+++
@@ -1,6 +0,0 @@
-const { default: axios } = require("axios");
-
-module.exports = (msg, args) => {
-  // Send a message reply to the same text channel as the message was sent from
-  msg.channel.send("Nothing coded yet, still in development, but you have passed these arguments: " + args);
-};
