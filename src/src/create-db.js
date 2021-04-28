f0c00fb1e0d919e0f6befdd8fb591b14a48c8104
Dovydas Paulauskas <47575688+dpaulo@users.noreply.github.com>
2021-04-28 11:21:59 +0300

    Revert "Crypto currency added all 3 commands"

--- src/src/create-db.js
+++
@@ -1,9 +0,0 @@
-const config = require("../config.json");
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(config.dbName);
-
-db.serialize(function () {
-  db.run("CREATE TABLE IF NOT EXISTS csgoPriceHistory (hash_name TEXT, user_id TEXT, lowest_price TEXT, median_price TEXT, last_checked TEXT NOT NULL)");
-});
-
-db.close();
