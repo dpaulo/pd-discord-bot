df33da81c6be09035d69fb86ff4e41dd08531ea7
Dovydas Paulauskas <dovydasp123@gmail.com>
2021-04-30 13:49:48 +0300

    Fixed typos in the error messages on Cryptocurrency and Exchange Rates features

--- src/components/exchange-rates/compare.js
+++ src/components/exchange-rates/compare.js
@@ -1,26 +1,21 @@
 const Discord = require("discord.js");
 const axios = require("axios");
 
-
-
 module.exports = async (message) => {
-    const [command, ...args] = message.content.split(' ');
-    // Getting data from price empire api
-    const [curCurrency, vsCurrency] = args;
-    try {
-        const { data } = await axios.get('https://public-api.pricempire.com/api/meta/getCurrencyRates');
-        var c = data[curCurrency]
-        var v = data[vsCurrency]
-        var result = v / c
-        result
-        // checking if data exists if not throw error
-        if (!data[curCurrency] || !data[vsCurrency]) throw Error();
-        return message.channel.send(` the current exchange rate of ${curCurrency} to ${vsCurrency} per 1 ${curCurrency} you will recieve ${result} ${vsCurrency}`)
-    }
-    catch (err) {
-        //error message telling the user to properly enter the command is properly entered
-        return message.channel.send(`Please make sure you correctly format the command and currencies, for example: $compare USD GBP`)
-
-
-    }
+  const [command, ...args] = message.content.split(" ");
+  // Getting data from price empire api
+  const [curCurrency, vsCurrency] = args;
+  try {
+    const { data } = await axios.get("https://public-api.pricempire.com/api/meta/getCurrencyRates");
+    var c = data[curCurrency];
+    var v = data[vsCurrency];
+    var result = v / c;
+    result;
+    // checking if data exists if not throw error
+    if (!data[curCurrency] || !data[vsCurrency]) throw Error();
+    return message.channel.send(`The current exchange rate of ${curCurrency} to ${vsCurrency} per 1 ${curCurrency} you will recieve ${result} ${vsCurrency}`);
+  } catch (err) {
+    //error message telling the user to properly enter the command is properly entered
+    return message.channel.send(`Please make sure you correctly format the command and currencies, for example: $compare USD GBP`);
+  }
 };
--- src/components/exchange-rates/convert.js
+++ src/components/exchange-rates/convert.js
@@ -2,24 +2,23 @@ const Discord = require("discord.js");
 const axios = require("axios");
 
 module.exports = async (message) => {
-    const [command, ...args] = message.content.split(' ');
-        //converting the parameters into their respective constants
-        const [amount, curCurrency, vsCurrency] = args;
-        try {
-            //getting data from the price empire api
-            const { data } = await axios.get('https://public-api.pricempire.com/api/meta/getCurrencyRates');
-            var x = data[curCurrency]
-            var y = data[vsCurrency]
-            // small calculation for the conversion of one currency to another
-            var result = (amount / x) * y
-            //rounding up to 2 decimal places to show how much money you would currently make 
-            result = result.toFixed(2)
-            // checking if all 3 parameters were properly entered if not throw error
-            if (!data[curCurrency] || !data[vsCurrency] || isNaN(result) == true) throw Error();
-            return message.channel.send(`The amount of ${amount} ${curCurrency} converted to ${vsCurrency} is ${result} ${vsCurrency} `)
-        }
-        catch (err) {
-            //error message telling the user to properly enter the command is properly entered
-            return message.channel.send(`please check you have correctly formatted the command and currencies, for example: $convert 100 USD GBP`)
-        }
-    };
+  const [command, ...args] = message.content.split(" ");
+  //converting the parameters into their respective constants
+  const [amount, curCurrency, vsCurrency] = args;
+  try {
+    //getting data from the price empire api
+    const { data } = await axios.get("https://public-api.pricempire.com/api/meta/getCurrencyRates");
+    var x = data[curCurrency];
+    var y = data[vsCurrency];
+    // small calculation for the conversion of one currency to another
+    var result = (amount / x) * y;
+    //rounding up to 2 decimal places to show how much money you would currently make
+    result = result.toFixed(2);
+    // checking if all 3 parameters were properly entered if not throw error
+    if (!data[curCurrency] || !data[vsCurrency] || isNaN(result) == true) throw Error();
+    return message.channel.send(`The amount of ${amount} ${curCurrency} converted to ${vsCurrency} is ${result} ${vsCurrency} `);
+  } catch (err) {
+    //error message telling the user to properly enter the command is properly entered
+    return message.channel.send(`Please check you have correctly formatted the command and currencies, for example: $convert 100 USD GBP`);
+  }
+};
